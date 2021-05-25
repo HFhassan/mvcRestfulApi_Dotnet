0d0d1d9bea031f56d75252f695e3963fac918c41
farooquiHassan <farooqui@codebtech.com>
2021-05-25 12:44:57 +0500

    setting up model and interface

---
+++ Data/MockComanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commads = new List<Command>
+            {
+                new Command{Id=0, HowTo ="Boil an egg", Line="Boil water",Platform ="Kettle & Pan"},
+                new Command{Id=1, HowTo ="cut the bread", Line="take a knife",Platform ="plate and dish"},
+                new Command{Id=2, HowTo ="have a trip", Line="take a car",Platform ="seaside or park"}
+            };
+
+            return commads;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" };
+        }
+    }
+}
